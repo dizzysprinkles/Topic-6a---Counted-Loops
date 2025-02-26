c1bca56ca417a6d4d60281403d6415c7829d5848
Zoey Hamm <339622581@GoTVDSB.ca>
2025-02-26 13:33:14 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Topic_6a___Counted_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++) //i++ is the same as i = i + 1 and i += 1
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+    }
+}
